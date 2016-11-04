e5fa1e5385025afd1d02635036a4b4e6dfa13d1d
minhthe <minhthe.007@gmail.com>
2016-11-04 13:41:46 +0700

    Display a list of users : connect to server Signed-off-by: minhthe <minhthe.007@gmail.com>

--- users.component.ts
+++ users.component.ts
@@ -1,10 +1,22 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {UsersService} from './users.service';
+// import {Observable} from 'rxjs/Observable';
 
 @Component({
-    // selector:'users',
-    template:`<h1>Users</h1>`
+    providers : [UsersService],
+    templateUrl :'app/users.component.html'
 })
 
-export class UsersComponent{
+export class UsersComponent implements OnInit{
+    users : any[] ;
+
+    constructor(private _usersService : UsersService){
+
+    }
+    
+    ngOnInit(){
+         this._usersService.getUsers().subscribe(users => this.users = users);
+       //  this._usersService.getUsers().subscribe(user => console.log(user));
+    }
 
 }
---
+++ users.service.ts
@@ -0,0 +1,20 @@
+import {Http} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import {Injectable} from 'angular2/core';
+import 'rxjs/add/operator/map';
+
+
+
+@Injectable()
+export class UsersService{
+    private _url = "https://jsonplaceholder.typicode.com/users";
+    constructor(private _http : Http){
+    }
+
+    getUsers(){
+        
+       return  this._http.get(this._url).map(user=>  user.json());
+        
+        
+    }
+}
